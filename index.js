9b7f2f3f715bcd082fa4dbd535721ae9b977aa2a
hyetigran <tigran.asriyan@gmail.com>
2020-01-14 01:24:24 +0400

    completed post endpoint for users

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // implement your API here
 const express = require("express");
+const cors = require("cors");
 
+const { find, findById, insert, remove, update } = require("./data/db");
 const server = express();
 
 server.get("/", (req, res) => {
@@ -8,7 +10,24 @@ server.get("/", (req, res) => {
 });
 
 server.post("/api/users", (req, res) => {
-  const { user, bio } = req.body;
+  const newUser = req.body;
+  if (!newUser.name || !newUser.bio) {
+    res
+      .status(400)
+      .json({ errorMessage: "Please provide name and bio for the user." });
+  } else {
+  insert(newUser)
+    .then(data => {
+        res.status(201).json(data);
+
+    })
+    .catch(error => {
+        res.status(500).json({
+            { errorMessage: "There was an error while saving the user to the database" }
+        })
+    });
+
+  }
 });
 
 server.listen(5000, () => console.log("API running on port 5000"));
