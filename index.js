7ae0c3a91aa8b4867023d3e731fd7603dd763b5f
hyetigran <tigran.asriyan@gmail.com>
2020-01-14 00:33:37 +0400

    installed dependencies and imported in index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // implement your API here
+const express = require("express");
+
+const server = express();
+
+server.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+server.post("/api/users", (req, res) => {
+  const { user, bio } = req.body;
+});
+
+server.listen(5000, () => console.log("API running on port 5000"));
