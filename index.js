3fe460113d253ee19ddabc1bd8c96da185544ea3
hyetigran <tigran.asriyan@gmail.com>
2020-01-14 13:28:11 +0400

    completed get all users and get user by id endpoints

--- index.js
+++ index.js
@@ -16,18 +16,49 @@ server.post("/api/users", (req, res) => {
       .status(400)
       .json({ errorMessage: "Please provide name and bio for the user." });
   } else {
-  insert(newUser)
-    .then(data => {
+    insert(newUser)
+      .then(data => {
         res.status(201).json(data);
+      })
+      .catch(error => {
+        res.status(500).json({
+          errorMessage:
+            "There was an error while saving the user to the database"
+        });
+      });
+  }
+});
 
+server.get("/api/users", (req, res) => {
+  find()
+    .then(data => {
+      res.status(200).json(data);
     })
     .catch(error => {
-        res.status(500).json({
-            { errorMessage: "There was an error while saving the user to the database" }
-        })
+      res.status(500).json({
+        errorMessage: "The users information could not be retrieved."
+      });
     });
+});
 
-  }
+server.get("/api/users/:id", (req, res) => {
+  const { id } = req.params;
+
+  findById(id)
+    .then(data => {
+      if (data) {
+        res.status(200).json(data);
+      } else {
+        res.status(404).json({
+          errorMessage: "The user with the specified ID does not exist."
+        });
+      }
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json({ errorMessage: "The user information could not be retrieved." });
+    });
 });
 
 server.listen(5000, () => console.log("API running on port 5000"));
